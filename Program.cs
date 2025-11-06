7f4e18bfa7913c80fac290b2bc9b43c61fc48e69
Brahian Montoya <johnbra696@gmail.com>
2025-11-05 21:25:00 -0500

    update MercadoPago service

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using ZenCloud.Data.DbContext;
+using ZenCloud.Data.Repositories.Implementations;
+using ZenCloud.Data.Repositories.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,8 +11,9 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
-// builder.Services.AddDbContext<PgDbContext>(options => 
-//     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<PgDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IPaymentRepository, PaymentRepository>();
 builder.Services.AddScoped<ZenCloud.Services.MercadoPagoService>();
 
 var app = builder.Build();
