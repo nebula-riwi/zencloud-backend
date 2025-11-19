3b46c79aed6a12e04eaf9210a0ae690cd1afcdc5
Jerogutiar <jeronimogutierrezarias@outlook.com>
2025-11-18 20:09:05 -0500

    feat(webhooks): Fix SecretToken column and add login/logout events
    - Create migration to fix SecretToken column type
    - Add UserLogin (10) and UserLogout (11) event types
    - Ready for production deployment

---
+++ Migrations/20251119010635_FixWebhookSecretTokenColumn.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZenCloud.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixWebhookSecretTokenColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
